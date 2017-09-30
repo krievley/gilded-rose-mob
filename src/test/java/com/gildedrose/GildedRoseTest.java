c0deb63a11d3d449795d662481100edbe6376cf0
Adam Zemmoura <adamzemmoura@gmail.com>
2017-09-30 21:19:28 +0400

    Tests for 2x quality and never negative quality

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -32,11 +32,27 @@ public class GildedRoseTest {
     }
 
     @Test
-    public void itemShould() {
-        app.items = new Item[] { new Item("item", 10, 10)};
+    public void itemQualityShouldDecrementTwiceAfterSell() {
+        app.items = new Item[] { new Item("item", -1, 10)};
         app.updateQuality();
 
-        assertEquals(9, app.items[0].sellIn);
+        assertEquals(8, app.items[0].quality);
+    }
+
+    @Test
+    public void itemQualityNeverNegative() {
+        app.items = new Item[] { new Item("item", 10, 0)};
+        app.updateQuality();
+
+        assertEquals(0, app.items[0].quality);
+    }
+
+    @Test
+    public void agedBrieIncreasesQuality() {
+        app.items = new Item[] { new Item("Aged Brie", 10, 0)};
+        app.updateQuality();
+
+        assertEquals(0, app.items[0].quality);
     }
 
 }
