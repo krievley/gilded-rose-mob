33260d56714a18f542272f045caea11a3adf5043
Adam Zemmoura <adamzemmoura@gmail.com>
2017-09-30 21:09:28 +0400

    Added sellIn decrement test

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -18,10 +18,25 @@ public class GildedRoseTest {
     @Test
     public void itemShouldDecrementQuality() {
         app.items = new Item[] { new Item("item", 10, 10)};
-
         app.updateQuality();
 
         assertEquals(9, app.items[0].quality);
+    }
+
+    @Test
+    public void itemShouldDecrementSellInValue() {
+        app.items = new Item[] { new Item("item", 10, 10)};
+        app.updateQuality();
+
+        assertEquals(9, app.items[0].sellIn);
+    }
+
+    @Test
+    public void itemShould() {
+        app.items = new Item[] { new Item("item", 10, 10)};
+        app.updateQuality();
+
+        assertEquals(9, app.items[0].sellIn);
     }
 
 }
